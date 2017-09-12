ecf64501aacb46d066614c3a616a75467607f967
mpedersen15 <mpedersen@mindrocketdesign.com>
2017-09-12 15:52:02 -0600

    Added in react-native-router-flux with initial route to Login

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import { View, Text  } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
-
 import firebase from 'firebase';
 
 import reducers from './reducers';
-import { Header } from './components/common';
-import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 class App extends Component {
     componentWillMount() {
@@ -28,10 +26,7 @@ class App extends Component {
         const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
         return (
             <Provider store={store}>
-                <View>
-                    {/* <Header headerText="Manager"/> */}
-                    <LoginForm />
-                </View>
+                <Router />
             </Provider>
         );
     }
